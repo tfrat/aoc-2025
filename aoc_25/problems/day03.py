3dc305bcc99c105d221e8434202d27be32451e4f
Tim Fratangelo <tfrat84@gmail.com>
2025-12-03 01:05:45 -0500

    Day 3

--- aoc_25/problems/day03.py
+++ aoc_25/problems/day03.py
@@ -2,19 +2,33 @@
 
 from __future__ import annotations
 
+from aoc_25.utils import chunk_by_blank_lines
+
+
+def find_total_voltage(banks: list[str], battery_size: int) -> int:
+    total = 0
+
+    for bank in banks:
+        digits = [0 for _ in range(battery_size)]
+        max_index = len(bank)
+        for i in range(battery_size - 1, -1, -1):
+            for idx, battery in enumerate(bank[::-1][i:max_index]):
+                value = int(battery)
+                if value >= digits[i]:
+                    digits[i] = value
+                    max_index = i + idx
+        total += sum(val * pow(10, i) for i, val in enumerate(digits))
+    return total
+
 
 class Day03:
-    """Placeholder implementation for Day 03."""
-
     day = 3
-    name = "Day 03 Placeholder"
+    name = "Day 03 Joltage"
 
     def solve_part_one(self, data: str) -> str:
-        return (
-            f"Day 03 part 1 not implemented (received {len(data.splitlines())} lines)"
-        )
+        [banks] = chunk_by_blank_lines(data)
+        return str(find_total_voltage(banks, 2))
 
     def solve_part_two(self, data: str) -> str:
-        return (
-            f"Day 03 part 2 not implemented (received {len(data.splitlines())} lines)"
-        )
+        [banks] = chunk_by_blank_lines(data)
+        return str(find_total_voltage(banks, 12))
