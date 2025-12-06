17ea883381fb9132d9ffa1be609811641a8a2cc5
Tim Fratangelo <tfrat84@gmail.com>
2025-12-06 00:15:19 -0500

    Day 6 Part 1

--- aoc_25/problems/day06.py
+++ aoc_25/problems/day06.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+from aoc_25.utils import parse_ints
+
 
 class Day06:
     """Placeholder implementation for Day 06."""
@@ -10,9 +12,17 @@ class Day06:
     name = "Day 06 Placeholder"
 
     def solve_part_one(self, data: str) -> str:
-        return (
-            f"Day 06 part 1 not implemented (received {len(data.splitlines())} lines)"
-        )
+        lines = data.splitlines()
+        operators = lines[-1].split()
+        results = parse_ints(lines[0])
+        for line in lines[1:-1]:
+            values = parse_ints(line)
+            for i, value in enumerate(values):
+                if operators[i] == "+":
+                    results[i] += value
+                else:
+                    results[i] *= value
+        return str(sum(results))
 
     def solve_part_two(self, data: str) -> str:
         return (
