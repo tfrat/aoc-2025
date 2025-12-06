258ae7fa23b7feb1cd3b120c720e606c93c74287
Tim Fratangelo <tfrat84@gmail.com>
2025-12-06 09:38:00 -0500

    Part 2 refactor

--- aoc_25/problems/day06.py
+++ aoc_25/problems/day06.py
@@ -25,25 +25,38 @@ class Day06:
     def solve_part_two(self, data: str) -> str:
         lines = data.splitlines()
         operators = lines[-1].split()
-        results = [0 if op == "+" else 1 for op in operators]
-        problem_index = 0
-        i = 0
-        stacks = defaultdict(list)
-        while i < len(lines[0]):
-            for j in range(0, len(lines) - 1):
-                stacks[i].append(lines[j][i])
+        num_cols = len(lines[0])
+
+        # Trim out the operators
+        lines = lines[0:-1]
+
+        # Loop over every character in the worksheet, building the vertical digits, keeping whitespace
+        columns = defaultdict(list)
+        for i in range(0, num_cols):
+            for j in range(0, len(lines)):
+                ch = lines[j][i]
+                if ch != " ":
+                    columns[i].append(int(ch))
             i += 1
-        for i in range(0, len(stacks)):
-            filtered = list(filter(lambda x: x != " ", stacks[i]))
-            collapsed = sum(
-                int(val) * pow(10, len(filtered) - x - 1)
-                for x, val in enumerate(filtered)
-            )
-            if not collapsed:
+
+        problem_index = 0
+        # Seed the results with the operator's identity value
+        results = [0 if op == "+" else 1 for op in operators]
+
+        for i in range(0, num_cols):
+            # We hit a column of pure white space, time to move on to the next problem
+            if not (column := columns[i]):
                 problem_index += 1
                 continue
+
+            # Build our number up MSB at the the, LSB at the bottom
+            number = sum(
+                int(val) * pow(10, len(column) - x - 1) for x, val in enumerate(column)
+            )
+
             if operators[problem_index] == "+":
-                results[problem_index] += collapsed
+                results[problem_index] += number
             else:
-                results[problem_index] *= collapsed
+                results[problem_index] *= number
+
         return str(sum(results))
